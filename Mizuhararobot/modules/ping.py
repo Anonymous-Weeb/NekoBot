88b16400955af95c2f44bd2434dd143b388c0a40
Anonymous-Weeb <75203373+Anonymous-Weeb@users.noreply.github.com>
2021-03-20 18:41:55 +0530

    update

--- Mizuhararobot/modules/ping.py
+++ Mizuhararobot/modules/ping.py
@@ -79,7 +79,7 @@ def ping(update: Update, context: CallbackContext):
     message.edit_text(
         "PONG!!\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>I am up since</b> <code>{}</code> nyan~~".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
